26b7590e50d1f562a914249bb670b4d2fca48000
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-08-23 18:29:01 +0200

    db config

---
+++ dbSetup/database.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize')
+
+const dbConfig = {
+    dialect:'sqlite',
+    host:'localhost',
+    storage:'./database.sqlite',
+    logging:false,
+}
+
+const sequelize = new Sequelize(
+    'database', 'user', 'password', dbConfig
+)
+
+module.exports = sequelize
