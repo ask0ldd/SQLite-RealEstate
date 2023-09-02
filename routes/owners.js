7d1fd1d0f29dd62abf946a09c4604a47513db80d
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-09-03 00:10:13 +0200

    owner controller

--- routes/owners.js
+++ routes/owners.js
@@ -2,12 +2,35 @@ const express = require('express')
 const Owner = require('../models/owner.model.js')
 const router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     try{
-        return res.status(200).json({id:'1', router:'owners', name:'doe', firstname:'john'})
+        const owners = await Owner.findAll();
+        console.log("All owners:", JSON.stringify(owners, null, 2));
+        return res.status(200).json(owners)
     } catch (error){
         console.error('Error finding the user:', error)
         res.status(500).json({ error: 'Internal server error' })
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try{
+        const owner = await Owner.findOne({id : req.params.id});
+        console.log("Owner:", JSON.stringify(owner, null, 2));
+        return res.status(200).json(owner)
+    } catch (error){
+        console.error('Error finding the user:', error)
+        res.status(500).json({ error: 'Internal server error' })
+    }
+})
+
+router.post('/', async (req, res) => {
+    try{
+        const owner = await Owner.create({firstname : req.owner.firstname, lastname : req.owner.firstname, picture : req.owner.picture})
+        console.log("Auto-generated ID:", owner.id);
+    }catch(error){
+        console.error('Error finding the user:', error)
+        res.status(500).json({ error: 'Internal server error' })        
     }
 })
 
